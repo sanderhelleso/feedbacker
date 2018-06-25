99d44d392bb4d9c78442875cbb8f7aa401dd0a17
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-25 23:17:42 +0200

    started up redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 // import React
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 // import App component from the App.js file
 import App from "./components/App";
+
+// starting up redux
+const store = createStore(() => [], {}, applyMiddleware());
 
 // render React App
 ReactDOM.render(<App />, document.querySelector("#root"));
