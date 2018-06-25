7d93cd2bf9823dd1321966be7de7ab0b177b80fa
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-25 19:25:46 +0200

    tweak strategy

--- services/passport.js
+++ services/passport.js
@@ -27,7 +27,8 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback"
+        callbackURL: "/auth/google/callback",
+        proxy: true
     }, (accesToken, refreshToken, profile, done) => {
 
         // check if user exists
