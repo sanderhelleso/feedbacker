819517912f55e6fdf26a1cafc1cee9907f251953
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-25 23:06:48 +0200

    first component

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+// import React
+import React from "react";
+
+// create App component
+const App = () => {
+    return (
+        <div>
+            Hi there!
+        </div>
+    );
+};
+
+// export App component
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+// import React
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import App component from the App.js file
+import App from "./components/App";
+
+// render React App
+ReactDOM.render(<App />, document.querySelector("#root"));
