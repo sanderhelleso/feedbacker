5962d9620676c22d5a9f4af7b81e8747981bc8fb
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-25 23:55:34 +0200

    made som basic dummy routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,24 @@
-// import React
+// import React & React router
 import React from "react";
+import { BrowserRouter, Route} from "react-router-dom";
+
+// dummy components
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+
 
 // create App component
 const App = () => {
     return (
         <div>
-            Hi there!
+            <BrowserRouter>
+                <div>
+                    <Route exact path="/" component ={Landing} />
+                    <Route path="/surveys" component ={Dashboard} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
