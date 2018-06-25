0d13b244d9688ca7e2bde8e24276853f5804c0c4
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-25 12:19:03 +0200

    adeed oauth

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 // require common modules
 const express = require("express");
 
+// passport & google oauth
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+
+// import keys module
+const keys = require("./config/keys");
+
 // initialize app as express
 const app = express();
 
-// route handlers 
-app.get("/", (req,res) => {
-    res.send({hi: "there"});
-});
+// create new instance of strategy
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: "/auth/google/callback"
+    }, accesToken => {
+        console.log(accesToken);
+    })
+);
+
+// google sign in route
+app.get(
+    "/auth/google", 
+    passport.authenticate("google", {
+        scope: ["profile", "email"]
+    })
+);
 
 // listen on port 2000
 const PORT = process.env.PORT || 2000;
