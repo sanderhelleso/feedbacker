8a0787118632475345b00cb00a769b4233ae4102
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-25 23:30:14 +0200

    added reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-// import React
+// import React, React-DOM & Redux
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -6,9 +6,10 @@ import { createStore, applyMiddleware } from "redux";
 
 // import App component from the App.js file
 import App from "./components/App";
+import reducers from "./reducers";
 
-// starting up redux store
-const store = createStore(() => [], {}, applyMiddleware());
+// starting up redux store reducer
+const store = createStore(reducers, {}, applyMiddleware());
 
 // render React App
 ReactDOM.render(
