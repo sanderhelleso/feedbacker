5ec18c80e3e18484c4b1582f1345f8921a856788
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-26 00:07:13 +0200

    splitted Header into new component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter, Route} from "react-router-dom";
 
 // dummy components
-const Header = () => <h2>Header</h2>
+import Header from "./Header";
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+// Header component
+class Header extends Component {
+    render() {
+        return(
+            <div>
+                Header
+            </div>
+        );
+    }
+}
+
+export default Header;
