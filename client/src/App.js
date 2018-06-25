010e66862c57d472e7f94cc4b5d968bddd7b8c3d
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-25 20:39:34 +0200

    added domain routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hi there</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <a href="/auth/google">Sign in with Google</a>
       </div>
     );
   }
