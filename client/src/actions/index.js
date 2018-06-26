56053cc1aa77525236ba0c7e53d78a78f812feb5
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-26 13:28:26 +0200

    refactored fetchUser

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,9 +2,7 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 // get request to /api/current_user
-export const fetchUser = () => {
-    return function(dispatch) {
-        axios.get("/api/current_user")
-        .then(res => dispatch({ type: FETCH_USER, payload: res }));
-    }
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get("/api/current_user")
+    dispatch({ type: FETCH_USER, payload: res });
 };
