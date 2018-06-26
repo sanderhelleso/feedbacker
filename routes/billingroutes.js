6e348780dbedd4a8961373d39de6ff54af86a7f0
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-26 20:42:00 +0200

    finished stripe setup

--- routes/billingroutes.js
+++ routes/billingroutes.js
@@ -1,9 +1,10 @@
+// import stripe and keys
 const keys = require("../config/keys")
-const stripe = require("stripe")();
+const stripe = require("stripe")(keys.stripeSecretKey);
 
 // handle payment routes
 module.exports = app => {
     app.post("/api/stripe", (req, res) => {
-
+        console.log(req.body);
     });
 };
