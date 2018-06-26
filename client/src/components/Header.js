1306be257b42c84a541f592227db24cb7c6186ba
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-26 18:55:57 +0200

    added stripe keys

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,7 +19,7 @@ class Header extends Component {
                 return <li><a href="/api/logout">Logout</a></li>;
         }
     }
-
+    
     // render function
     render() {
         return(
