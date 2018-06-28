288237ef4511de0a8206ade65841c47b32abec49
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-28 14:53:40 +0200

    updated schema

--- models/Survey.js
+++ models/Survey.js
@@ -8,8 +8,10 @@ const surveySchema = new Schema({
     subject: String,
     recipients: [RecipientSchema],
     yes: { type: Number, default: 0},
-    no: { type: Number, default: 0}
-
+    no: { type: Number, default: 0},
+    _user: { type: Schema.Types.ObjectId, ref: "User" },
+    dataSet: Date,
+    lastResponded: Date
 });
 
 // set model
