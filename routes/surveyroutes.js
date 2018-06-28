c005d822694bb5c4a2c2510bfed1a02d369046ad
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-28 18:20:18 +0200

    updated survey model

--- routes/surveyroutes.js
+++ routes/surveyroutes.js
@@ -1,8 +1,20 @@
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 
-module.exports = app => {
-    app.post("/api/surveys", requireLogin, (req, res) => {
+const Surveys = mongoose.model("surveys");
 
+module.exports = app => {
+    app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+        const { title, subject, body, recepients } = req.body;
+
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recepients: recepients.split(",").map(email => ({ email: email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now()
+        });
     });
 };
