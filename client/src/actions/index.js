c77a34390c0d34f40dda380c6060c9de01420348
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-02 18:48:49 +0200

    added action for fetching surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEYS } from "./types";
 
 // get request to /api/current_user
 export const fetchUser = () => async dispatch => {
@@ -18,4 +18,10 @@ export const submitSurvey = (values, history) => async dispatch => {
     const res = await axios.post("/api/surveys", values);
     history.push("/surveys"); // return to dashboard AFTER survey is sent
     dispatch({ type: FETCH_USER, payload: res.data });
-};  
+}; 
+
+// get all surveys
+export const fetchSurveys = () =>async dispatch => {
+    const res = await axios.get("/api/surveys");
+    dispatch({ type: FETCH_SURVEYS, payload: res.data });
+};
